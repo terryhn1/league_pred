a9829ee8fc7e34e552c5435c3f078532b02a845e
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2021-06-07 22:57:12 -0700

    2.4
    Accuracy written. No confidence yet.

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ def testConditions(model, valid):
     sumElim = lambda F, Xlist: F.sum(Xlist)
     limit = 10
     iter = 0
+    accuracy = 0
     for xj in valid:
         conditions = {model.X[0]: xj[0], model.X[1]: xj[1], model.X[2]: xj[2], model.X[3]: xj[3]
                     , model.X[4]: xj[4], model.X[5]: xj[5], model.X[6]: xj[6], model.X[7]: xj[7]
@@ -16,22 +17,24 @@ def testConditions(model, valid):
 
         #Calculates the probability of win condition based on the evidence seen
         model.eliminate(model.X[:-1], sumElim)
-        print(model.factors[-1].table/ model.factors[-1].table.sum())
+        probs = list(enumerate(model.factors[-1].table/ model.factors[-1].table.sum()))
+        winningPred = max(probs,key = lambda x: x[1])
+
+        #If the winning prediction matches with the data
+        if xj[10] == winningPred[0]:
+            accuracy += 1
+
         #Reset the original model and the reset variable for every iteration
         model = reset
         reset = copy.deepcopy(model)
-
-        if iter == limit:
-            break
-        else:
-            iter += 1
     
-    return
+    return accuracy /len(valid)
 
 
 if __name__ == "__main__":
     model, factor_list = factors.initializeGraphModel()
     
     model, valid = factors.loadData(model, factor_list)
-    testConditions(model, valid)
+    accuracy = testConditions(model, valid)
+    print(accuracy)
     
